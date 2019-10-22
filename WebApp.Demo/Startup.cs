2e0707217d21d40cb6640f8cca6231e57260fa6d
kingime90 <kingime90@163.com>
2019-10-23 00:16:18 +0800

    WebApp.Demo

---
+++ WebApp.Demo/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Owin;
+using Microsoft.Owin;
+using WebApp.Demo;
+using NShine.Core.Dependency;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace WebApp.Demo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            IServiceBuilder builder = new ServiceBuilder();
+            IServiceCollection services = builder.Build();
+
+        }
+    }
+}
